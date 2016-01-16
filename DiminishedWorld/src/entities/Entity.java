210779d3f479c626ac3cf91316fb13571fb7e309
Dillon <Dillonmwall@msn.com>
2016-01-15 19:05:09 -0800

    Fixed everything to be like before

--- DiminishedWorld/src/entities/Entity.java
+++ DiminishedWorld/src/entities/Entity.java
@@ -7,7 +7,7 @@ import org.lwjgl.util.vector.Vector3f;
 public class Entity {
 
 	private TexturedModel model;
-	private Vector3f position;
+	protected Vector3f position;
 	private float rotX, rotY, rotZ;
 	private float scale;
 	
--- DiminishedWorld/src/entities/Player.java
+++ DiminishedWorld/src/entities/Player.java
@@ -1,74 +1,83 @@
 package entities;
 
-import models.TexturedModel;
-
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
 
+import models.TexturedModel;
 import renderEngine.DisplayManager;
-import terrains.Terrain;
 
 public class Player extends Entity {
 
-	private static final float RUN_SPEED = 40;
-	private static final float TURN_SPEED = 160;
-	private static final float GRAVITY = -50;
-	private static final float JUMP_POWER = 18;
+	private static final float MOVE_SPEED = 50;
+	private static final float SPRINT_MULT = 2;
+	private static final float TURN_SPEED = 2f;
 
-	private float currentSpeed = 0;
-	private float currentTurnSpeed = 0;
-	private float upwardsSpeed = 0;
+	private float speed = MOVE_SPEED;
 
-	private boolean isInAir = false;
+	float dx = 0;
+	float dz = 0;
+	float dy = 0;
 
-	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ,
+	private Camera camera;
+
+	public Player(Camera camera, TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ,
 			float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
+		this.camera = camera;
+		camera.setPlayer(this);
 	}
 
-	public void move(Terrain terrain) {
+	public void move() {
+		speed = MOVE_SPEED * DisplayManager.getFrameTimeSeconds();
 		checkInputs();
-		super.increaseRotation(0, currentTurnSpeed * DisplayManager.getFrameTimeSeconds(), 0);
-		float distance = currentSpeed * DisplayManager.getFrameTimeSeconds();
-		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
-		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
-		super.increasePosition(dx, 0, dz);
-		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
-		super.increasePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
-		float terrainHeight = terrain.getHeightOfTerrain(getPosition().x, getPosition().z);
-		if (super.getPosition().y < terrainHeight) {
-			upwardsSpeed = 0;
-			isInAir = false;
-			super.getPosition().y = terrainHeight;
-		}
-	}
-
-	private void jump() {
-		if (!isInAir) {
-			this.upwardsSpeed = JUMP_POWER;
-			isInAir = true;
-		}
 	}
 
 	private void checkInputs() {
-		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			this.currentSpeed = RUN_SPEED;
-		} else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			this.currentSpeed = -RUN_SPEED;
-		} else {
-			this.currentSpeed = 0;
-		}
+		float yaw = -90 - this.getRotY();
+		float pitch = this.getRotZ();
 
-		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			this.currentTurnSpeed = -TURN_SPEED;
-		} else if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			this.currentTurnSpeed = TURN_SPEED;
-		} else {
-			this.currentTurnSpeed = 0;
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+			speed *= SPRINT_MULT;
 		}
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
-			jump();
+		if (camera.getType() == Camera.FIRST_PERSON || camera.getType() == Camera.THIRD_PERSON) {
+			if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+				dz = (float) (speed * Math.cos(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch - 90)));
+				dx = (float) (-speed * Math.sin(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch - 90)));
+				dy = (float) (-speed * Math.sin(Math.toRadians(pitch))); // correct
+				position.z += dz;
+				position.x += dx;
+				position.y += dy;
+				if (camera.getType() == Camera.THIRD_PERSON) {
+					if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+						this.setRotY(this.getRotY() - TURN_SPEED);
+					}
+					if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+						this.setRotY(this.getRotY() + TURN_SPEED);
+					}
+				}
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+				dz = (float) (-speed * Math.cos(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch - 90)));
+				dx = (float) (speed * Math.sin(Math.toRadians(yaw)) * Math.sin(Math.toRadians(pitch - 90)));
+				dy = (float) (speed * Math.sin(Math.toRadians(pitch)));
+				position.z += dz;
+				position.x += dx;
+				position.y += dy;
+				if (camera.getType() == Camera.THIRD_PERSON) {
+					if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+						this.setRotY(this.getRotY() - TURN_SPEED);
+					}
+					if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+						this.setRotY(this.getRotY() + TURN_SPEED);
+					}
+				}
+			}
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
+			this.setRotZ(this.getRotZ() + 1f);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_F)) {
+			this.setRotZ(this.getRotZ() - 1f);
 		}
 	}
 
